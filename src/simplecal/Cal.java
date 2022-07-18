1b8714ef8ca5e27c26ff3ca4b27c76a6d352c20a
Sonu Saini <sonusainicse@gmail.com>
2022-07-18 18:49:40 +0530

    second commit

--- src/simplecal/Cal.java
+++ src/simplecal/Cal.java
@@ -1,58 +1,14 @@
 package simplecal;
-import java.util.Scanner;
 
 public class Cal {
 
 	  public static void main(String[] args) {
-
-	    char operator;
-	    Double number1, number2, result;
-
-	    // create an object of Scanner class
-	    Scanner input = new Scanner(System.in);
-
-	    // ask users to enter operator
-	    System.out.println("Choose an operator: +, -, *, or /");
-	    operator = input.next().charAt(0);
-
-	    // ask users to enter numbers
-	    System.out.println("Enter first number");
-	    number1 = input.nextDouble();
-
-	    System.out.println("Enter second number");
-	    number2 = input.nextDouble();
-
-	    switch (operator) {
-
-	      // performs addition between numbers
-	      case '+':
-	        result = number1 + number2;
-	        System.out.println(number1 + " + " + number2 + " = " + result);
-	        break;
-
-	      // performs subtraction between numbers
-	      case '-':
-	        result = number1 - number2;
-	        System.out.println(number1 + " - " + number2 + " = " + result);
-	        break;
-
-	      // performs multiplication between numbers
-	      case '*':
-	        result = number1 * number2;
-	        System.out.println(number1 + " * " + number2 + " = " + result);
-	        break;
-
-	      // performs division between numbers
-	      case '/':
-	        result = number1 / number2;
-	        System.out.println(number1 + " / " + number2 + " = " + result);
-	        break;
-
-	      default:
-	        System.out.println("Invalid operator!");
-	        break;
-	    }
-
-	    input.close();
+	    Double number1=50.0, number2=5.0, result, result1;
+	    result= number1*number2;
+ 
+	    System.out.println("Multiplication "+result);
+	    
+	    result1=number1/number2;
+	    System.out.println("Division "+result1);
 	  }
 	}
